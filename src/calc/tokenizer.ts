85f5727e52da5807a842e2af0677cda03976e1b7
carbonsoda <carbonsoda@outlook.com>
2025-02-06 18:44:17 +0800

    new tokenizer

--- src/calc/tokenizer.ts
+++ src/calc/tokenizer.ts
@@ -1,109 +1,62 @@
-import { BinaryOperatorValues, BracketValues, Token, TokenTypes, TokenTypesCnt, UnaryOperatorValues } from "./types";
+import { Registry } from "./registry";
 
-/**
- * @param input raw input string
- * @returns lexical tokens of {@link TokenTypes}
- */
-export function tokenize(input: string): Token<TokenTypes>[] {
-	const length = input.length;
-
-	const tokens: Token<TokenTypes>[] = [];
-	while (input.length) {
-		const index = length - input.length;
-
-		const match = digest(input, tokens, index);
-		if (!match) throw new SyntaxError(`Unknown symbol at index ${index}.`);
-
-		input = input.slice(match.length);
+export namespace Tokenizer {
+	export enum TokenTypes {
+		Bracket,
+		Numeral,
+		UnaryOp,
+		BinaryOp,
 	}
-	return tokens;
-}
 
-const TokenTypesRE = {
-	[TokenTypes.Bracket]: /[\(\)]/,
-	[TokenTypes.UnaryOperator]: /[\+\-]/,
-	[TokenTypes.BinaryOperator]: /[\+\-\*\/\^]/,
-	[TokenTypes.Numeral]: /\d+(?:\.\d+)?(?:[eE](?:[\+\-])?\d+)?/,
-};
+	export class Token {
+		constructor(public type: TokenTypes, public symbol: string, public from: number, public to: number) {}
 
-/**
- * Intake input string and push tokens into `tokens`, with position index information
- * @returns raw match string digested
- */
-function digest(input: string, tokens: Token<TokenTypes>[], index: number): string | undefined {
-	let match: string | undefined;
-	for (let i = 0; i < TokenTypesCnt; i++) {
-		const tokenTypeRE = new RegExp(`^\\s*${TokenTypesRE[<keyof typeof TokenTypesRE>i].source}\\s*`);
-		match = input.match(tokenTypeRE)?.[0];
-		if (!match) continue;
-
-		const literal = match.trim();
-		switch (i) {
-			case TokenTypes.Bracket: {
-				const bracketValue = literal === "(" ? BracketValues.Left : BracketValues.Right;
-				const last = tokens.at(-1);
-				if (
-					(last?.type === TokenTypes.Numeral ||
-						(last?.type === TokenTypes.Bracket && last.value === BracketValues.Right)) &&
-					bracketValue === BracketValues.Left
-				) {
-					tokens.push({
-						type: TokenTypes.BinaryOperator,
-						value: BinaryOperatorValues.Mul,
-						index,
-					});
-				}
-				tokens.push({
-					type: TokenTypes.Bracket,
-					value: bracketValue,
-					index,
-				});
-				break;
-			}
-
-			// @ts-ignore intended fallthrough case if token is actually binary +/-
-			case TokenTypes.UnaryOperator: {
-				const last = tokens.at(-1);
-				if (
-					last?.type === undefined ||
-					(last?.type === TokenTypes.Bracket && last.value === BracketValues.Left) ||
-					last?.type === TokenTypes.UnaryOperator ||
-					last?.type === TokenTypes.BinaryOperator
-				) {
-					tokens.push({
-						type: TokenTypes.UnaryOperator,
-						value: literal === "-" ? UnaryOperatorValues.Neg : UnaryOperatorValues.Pos,
-						index,
-					});
-					break;
-				}
-			}
-
-			case TokenTypes.BinaryOperator: {
-				tokens.push({
-					type: TokenTypes.BinaryOperator,
-					value: {
-						"+": BinaryOperatorValues.Add,
-						"-": BinaryOperatorValues.Sub,
-						"*": BinaryOperatorValues.Mul,
-						"/": BinaryOperatorValues.Div,
-						"^": BinaryOperatorValues.Pow,
-					}[<"+" | "-" | "*" | "/" | "^">literal],
-					index,
-				});
-				break;
-			}
-
-			case TokenTypes.Numeral: {
-				tokens.push({
-					type: TokenTypes.Numeral,
-					value: Number(literal),
-					index,
-				});
-				break;
-			}
+		toString(): string {
+			return `Token[${this.from}-${this.to}]<${TokenTypes[this.type]}>`.padEnd(30) + this.symbol;
 		}
-		break;
 	}
-	return match;
+
+	export function tokenize(input: string): Token[] {
+		const length = input.length;
+		const tokens: Token[] = [];
+		while (input.length) {
+			const index = length - input.length;
+			const match = digest(input, index, tokens);
+			input = input.slice(match.length);
+		}
+		return tokens;
+	}
+
+	function escapeSymbolForRE(pattern: string): string {
+		return pattern.replaceAll(/[.*+?^${}()|[\]\\]/g, "\\$&");
+	}
+	const BRACKET_TOKEN_RE = /[()]/;
+	const NUMERAL_TOKEN_RE = new RegExp(
+		`(?:${Registry.getConstantSymbols().map(escapeSymbolForRE).join(")|(?:")})|` +
+			`(?:\\d+(?:\\.\\d+)?(?:[eE][+-]?\\d+)?)`,
+	);
+	const UNARYOP_TOKEN_RE = new RegExp(`(?:${Registry.getUnaryOpSymbols().map(escapeSymbolForRE).join(")|(?:")})`);
+	const BINARYOP_TOKEN_RE = new RegExp(`(?:${Registry.getBinaryOpSymbols().map(escapeSymbolForRE).join(")|(?:")})`);
+	const TOKEN_TYPES_RE: { [type: number]: RegExp } = {
+		[TokenTypes.Bracket]: BRACKET_TOKEN_RE,
+		[TokenTypes.Numeral]: NUMERAL_TOKEN_RE,
+		[TokenTypes.UnaryOp]: UNARYOP_TOKEN_RE,
+		[TokenTypes.BinaryOp]: BINARYOP_TOKEN_RE,
+	};
+
+	function digest(input: string, index: number, tokens: Token[]): string {
+		let match: string | undefined;
+		const TOKEN_TYPE_CNT = Object.keys(TokenTypes).length / 2;
+		for (let type: TokenTypes = 0; type < TOKEN_TYPE_CNT; type++) {
+			const tokenTypeRE = new RegExp(`^\\s*(?:${TOKEN_TYPES_RE[type].source})\\s*`);
+			match = input.match(tokenTypeRE)?.[0];
+			if (match === undefined) continue;
+
+			const token = new Token(type, match.trim(), index, index + match.length);
+			tokens.push(token);
+			break;
+		}
+		if (!match) throw new SyntaxError(`Unknown symbol at index ${index}.`);
+		return match;
+	}
 }
